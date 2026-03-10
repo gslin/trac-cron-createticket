d033b70df37d80f37e4737d070879065515e520b
Gea-Suan Lin <gslin@gslin.com>
2026-03-11 03:40:31 +0800

    chore: bump version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='TracCronCreateTicket',
-    version='1.0.0',
+    version='1.0.1',
     description='Trac plugin for scheduled ticket creation',
     author='Your Name',
     author_email='your@email.com',
